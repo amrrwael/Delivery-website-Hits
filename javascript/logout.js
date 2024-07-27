1b5473fe5e4b73790b36b939c03ef6dbdbe15351
Amr Wael <93450935+amrrwael@users.noreply.github.com>
2024-07-27 21:40:14 +0700

    Update logout.js

--- javascript/logout.js
+++ javascript/logout.js
@@ -1,37 +1,25 @@
 function logout() {
-    const apiUrl = 'https://food-delivery.int.kreosoft.space/api/account/logout';
-
     const token = localStorage.getItem('token');
 
-    if (!token) {
-        console.log('Token not found. User might not be logged in.');
-        return;
+    if (token) {
+        fetch('https://food-delivery.int.kreosoft.space/api/account/logout', {
+            method: 'POST',
+            headers: {
+                'Authorization': `Bearer ${token}`,
+                'Content-Type': 'application/json'
+            }
+        })
+        .then(response => {
+            if (response.ok) {
+                localStorage.removeItem('token');
+                alert("You have successfully logged out.");
+                location.reload();
+            } else {
+                throw new Error('Logout failed');
+            }
+        })
+        .catch(error => {
+            console.error("Error logging out:", error);
+        });
     }
-
-    var headers = {
-        'Content-Type': 'application/json',
-        'Authorization': `Bearer ${token}`
-    };
-
-    fetch(apiUrl, {
-        method: 'POST',
-        headers: headers
-    })
-    .then(response => {
-        console.log('Logout Response:', response);
-    
-        if (response.ok) {
-            console.log('Logout successful');
-            localStorage.removeItem('token');
-            window.location.href = '/';
-        } else {
-            console.log('Logout failed:', response.status);
-            // Log the response body for more details
-            response.text().then(text => console.log('Response body:', text));
-        }
-    })
-    
-    .catch(error => {
-        console.error('Error:', error);
-    });
 }
